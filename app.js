516bd03c91801fa04e33ecd25526009525431bf2
Lukasz Grzegorzewski <grzegorzewski.luk@gmail.com>
2022-11-06 17:22:58 +0100

    style 404 page Not Found and button

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
   } else {
-    console.log(`Server is listening on ${port}`);
+    console.log(`Server is listening on port : ${port}`);
   }
 });
--- userHandlers.js
+++ userHandlers.js
@@ -11,12 +11,18 @@ const getUsers = (req, res) => {
 
 const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
+  const image404 = "https://img.freepik.com/premium-vector/error-404-illustration_585024-2.jpg?w=740";
+
   database
     .query("SELECT * FROM users WHERE id = ?", [id])
     .then(([user]) => {
-      user[0] != null ?
-        res.status(200).json(user[0]) :
-        res.status(404).send("Not Found")
+      if (user[0] != null){
+        res.status(200).json(user[0]);
+      } else {
+        res.write("<div><h1 style='text-align:center;'>Not Found</h1><a href='/api/users'><button style='position: absolute; left: calc(50% - 50px); height: 30px; width: 100px; border:none; box-shadow: 3px 3px 5px rgba(0, 0, 0, .5);'> <<< USERS</button></a></div>");
+        res.write("<img src=" + image404 + " style='width: 100vw;'></img>");
+        res.status(404).send();
+      }
     })
     .catch((err) => console.error(err))
 };

0e78c41805ce897e4eedfd12b07a5b4710000d8c
Lukasz Grzegorzewski <grzegorzewski.luk@gmail.com>
2022-11-06 15:05:22 +0100

    Initial git

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const movieHandlers = require("./movieHandlers");
+
+const app = express();
+
+const port = process.env.APP_PORT ?? 5001;
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
+
+
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", )
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
